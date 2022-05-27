0f7a3c7e71bfc400ca1e0de325063d28b242433e
egiadriansyah <egiadr156@gmail.com>
2022-05-28 04:29:26 +0700

    part 2

--- app.py
+++ app.py
@@ -29,12 +29,75 @@
 # print(age)
 
 # course = ("python's for beginner")
+# print(course[-2])
+
+# course = ('''
+# Hi, Egi Adriansyah
+# Keep coding
+# if braind empty
+# '''
+# )
 # print(course)
 
-course = ('''
-Hi, Egi Adriansyah
-Keep coding
-if braind empty
-'''
-)
-print(course)
+# course = 'Python for beginners'
+# print(course.upper())
+# print(course.lower())
+# print(course)
+# print(course.find('e'))
+# print('Python' in course)
+# print('python' in course)
+
+# print (course.replace('P','J'))
+
+# print(10 % 3) #Aritmatika
+# print(10 / 3)
+# print(10 ** 3)
+# x = 3
+# y = 10
+# print(y * x)
+
+# math function
+# import math
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+#
+# x = 2.9
+# print(round(x))
+# print(abs(-2.9))
+
+# if statement
+# is_hot = True #Truee
+# is_cold = False
+# if is_hot :
+#     print("it's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("enjoy yours day")
+#     print("enjoy life")
+# else:
+#     print("it's a lovely day")
+# price = 100000
+# has_good_credit = True
+#
+# if has_good_credit:
+#     down_payment = 0.1 * price
+# else :
+#     down_payment = 0.2 * price
+# print(f"down payment: {down_payment}")
+
+# penghasilan_tinggi = False
+# saldo_baik = True
+#
+# if penghasilan_tinggi or saldo_baik:
+#     print("kamu hebat:)")
+
+# has_good_credit = False
+# has_criminal_record = True
+# if has_good_credit and not has_criminal_record:
+#     print("eligible for loan")
+
+# temperatur = 35 #30
+# if temperatur > 30:
+#     print("it's a hot day")
+# else:
+#     print("it's not a hot day")
