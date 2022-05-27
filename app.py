19da4c91ba4f04bf3cf52a5d8ad2745ed0d5d493
egiadriansyah <egiadr156@gmail.com>
2022-05-27 22:12:44 +0700

    Belajar Phython

---
+++ app.py
@@ -0,0 +1,40 @@
+# print('Egi Adriansyah')
+# print('0----')
+# print(' ||||')
+# print('*' * 10)
+
+# variable
+# harga = 10
+# rating = 4.9
+# name = 'adr'
+# is_published = False
+# print(harga)
+# print(rating)
+# print(name)
+# print(is_published)
+# full_name = 'Egi Adriansyah'
+# age = 23
+# is_new = True
+# print(full_name)
+# print(age)
+# print(is_new)
+
+# input
+# name = input('What is your name? ')
+# favorite_color = input('What is your favorite? ')
+# print(name + 'Like ' + favorite_color)
+# Type coversial
+# birth_year = input('Birth year: ')
+# age = 2022 - int(birth_year)
+# print(age)
+
+# course = ("python's for beginner")
+# print(course)
+
+course = ('''
+Hi, Egi Adriansyah
+Keep coding
+if braind empty
+'''
+)
+print(course)
